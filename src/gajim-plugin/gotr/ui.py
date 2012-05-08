4e91e244deaec2d5a4b947fcde937632bd1b087d
Kjell Braden <afflux@pentabarf.de>
2012-05-08 21:38:03 +0200

    woops, commited testing code. choosing default clipboard for consistency

--- src/gajim-plugin/gotr/ui.py
+++ src/gajim-plugin/gotr/ui.py
@@ -98,7 +98,7 @@ class OtrPluginConfigDialog(GajimPluginConfigDialog):
             it = mod.get_iter(path)
             jid, fpr = mod.get(it, 0, 6)
             fprs.append('%s: %s' % (jid, potr.human_hash(fpr)))
-        gtk.Clipboard(selection='PRIMARY').set_text('\n'.join(fprs))
+        gtk.Clipboard().set_text('\n'.join(fprs))
         
     def flags_toggled_cb(self, button):
         if button == self.B.get_object('enable_check'):
