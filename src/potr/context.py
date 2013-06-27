d2f99cd01da7b1b2ad26c184101c70734658ab1d
Kjell Braden <afflux@pentabarf.de>
2013-06-28 01:01:25 +0200

    drop unused slot

--- src/potr/context.py
+++ src/potr/context.py
@@ -64,7 +64,7 @@ OFFER_ACCEPTED = 3
 class Context(object):
     __slots__ = ['user', 'policy', 'crypto', 'tagOffer', 'lastSend',
             'lastMessage', 'mayRetransmit', 'fragment', 'fragmentInfo',
-            'state', 'trust', 'peer', 'trustName']
+            'state', 'peer', 'trustName']
 
     def __init__(self, account, peername):
         self.user = account
